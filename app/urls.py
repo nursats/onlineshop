6e513c0a061d023cfeffe52c4b84d2b81e4f7266
Nursat Seitov <nursat.seitov12@gmail.com>
2024-03-22 17:15:53 +0500

    media, database

--- app/urls.py
+++ app/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 from app.settings import DEBUG
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,7 +25,9 @@ urlpatterns = [
     path('catalog/', include('goods.urls', namespace='catalog')),
 ]
 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns += [
         path("__debug__/", include("debug_toolbar.urls")),
+
         ]
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
